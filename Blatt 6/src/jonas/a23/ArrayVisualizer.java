1494331af4ed91378b9d47b3286e84b9a5beab81
Jonas <xyxyxyz@hotmail.de>
2013-05-26 20:43:22 +0200

    comments mini fixes

--- Blatt 6/src/jonas/a23/ArrayVisualizer.java
+++ Blatt 6/src/jonas/a23/ArrayVisualizer.java
@@ -1,11 +1,22 @@
 package jonas.a23;
 
+/**
+ * Uebungsgruppe G04-C Henner Niehaus, Jonas Stadler, Eva Poell
+ *
+ * Testklasse zu Aufgabe 16, Blatt 2
+ *
+ */
 import java.awt.*;
 import java.awt.image.*;
 import javax.swing.*;
 
 public class ArrayVisualizer extends JPanel {
 
+	/**
+	 * serial Version
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private Integer[] data;
 
 	public ArrayVisualizer(int n) {
@@ -17,20 +28,23 @@ public class ArrayVisualizer extends JPanel {
 		data = a;
 	}
 
+	/**
+	 * Zeichnet zu jedem Wert aus dem Integer-Array einen einzelnen Punkt
+	 */
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
 		for (int i = 0; i < data.length; i++) {
-//			g.drawString(".", data.length - i, data[i]);
-			g.drawLine(i, data.length-data[i], i, data.length);
+			 g.drawString(".", data.length - i, data[i]);// zeichnet zu jedem Wert von data einen punkt auf die zeichenflaeche.
+			//g.drawLine(i, data.length - data[i], i, data.length); // zeichnet einen balken vom unteren ender der zeichenfläche bis zum wert
 		}
 
 	}
 
 	public void update() {
-		BufferedImage b = new BufferedImage(data.length, data.length, BufferedImage.TYPE_INT_ARGB);
-		paintComponent(b.getGraphics());
-
+		BufferedImage image = new BufferedImage(data.length, data.length, BufferedImage.TYPE_INT_ARGB); //eryeugt ein bild um graphics zu beommen
+		paintComponent(image.getGraphics());
 	}
 
 }
