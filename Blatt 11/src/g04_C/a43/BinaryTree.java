72eb553e6fd70ce57064faeac19a1c441f73681b
Jonas <xyxyxyz@hotmail.de>
2013-06-28 20:14:19 +0200

    a43 fertig alle test bestanden

--- Blatt 11/src/g04_C/a43/BinaryTree.java
+++ Blatt 11/src/g04_C/a43/BinaryTree.java
@@ -1,99 +1,118 @@
 package g04_C.a43;
 
 /**
- *  Eine Moeglichkeit, einen binaeren Baum zu modellieren.
- *  Diese Modellierung entspricht der induktiven Definition 6.1
- *  Aufgabe 43 / Blatt 11 / Sommersemester 2013
+ * Eine Moeglichkeit, einen binaeren Baum zu modellieren. Diese Modellierung entspricht der induktiven Definition 6.1 Aufgabe 43 / Blatt 11 / Sommersemester 2013
  */
 public class BinaryTree<T> {
 
-    private BinaryTree<T> leftChild;   //  Referenz auf linken Teilbaum.
-    private BinaryTree<T> rightChild;  //  Referenz auf rechten Teilbaum.
-    private T data;                    //  Datenfeld
+	private BinaryTree<T> leftChild; // Referenz auf linken Teilbaum.
+	private BinaryTree<T> rightChild; // Referenz auf rechten Teilbaum.
+	private T data; // Datenfeld
+	private String space = ""; // Spacer fuer Ausgabe
 
-    /**
-     *  Konstruktor fuer einen Teilbaum ohne Kinder.
-     *  @param data Referenz auf das im Datenfeld zu speichernde Objekt.
-     */
-    BinaryTree(T data) {
-	this(data, null, null);
-    }
-
-    /**
-     *  Konstruktor fuer einen Teilbaum ohne Kinder.
-     *  @param data Referenz auf das im Datenfeld zu speichernde Objekt.
-     *  @param leftChild Referenz auf den linken Teilbaum.
-     *  @param rightChild Referenz auf den rechten Teilbaum.
-     */
-    BinaryTree(T data, BinaryTree<T> leftChild, BinaryTree<T> rightChild) {
-        this.leftChild  = leftChild;
-        this.rightChild = rightChild;
-        this.data       = data;
-    }
-
-    /**
-     *  Gibt eine Referenz auf das im Datenfeld verwaltete Objekt zurueck.
-     *  @return Referenz auf das im Datenfeld verwaltete Objekt.
-     */
-    public T getData() {
-	return data;
-    }
-    
-    /**
-     *  Speichert die uebergebene Referenz im Datenfeld.
-     *  @param Neu zu speicherende Objektreferenz.
-     */
-    public void setData(T data) {
-	this.data = data;
-    }
-
-    /**
-     *  Gibt eine Referenz auf den linken Teilbaum zurueck.
-     *  @return Referenz auf den linken Teilbaum.
-     */
-    public BinaryTree<T> getLeftChild() {
-	return leftChild;
-    }
-    
-    /** 
-     *  Setzt einen neuen linken Teilbaum.
-     *  @param Referenz auf den neuen linken Teilbaum.
-     */
-    public void setLeftChild(BinaryTree<T> leftChild) {
-	this.leftChild = leftChild;
-    }
-    
-    /**
-     *  Gibt eine Referenz auf den rechten Teilbaum zurueck.
-     *  @return Referenz auf den rechten Teilbaum.
-     */
-    public BinaryTree<T> getRightChild() {
-	return rightChild;
-    }
-    
-    /** 
-     *  Setzt einen neuen rechten Teilbaum.
-     *  @param Referenz auf den neuen rechten Teilbaum.
-     */
-    public void setRightChild(BinaryTree<T> rightChild) {
-	this.rightChild = rightChild;
-    }
-
-    /**
-     *  Erzeugt eine "balanced parentheses"-Repraesentation des
-     *  am aktuellen Knoten gespeicherten Teilbaums inklusive der
-     *  String-Repraesentation des an der Wurzel verwalteten Datenobjekts.
-     *  @return Textuelle Darstellung der "balanced parentheses"-Repraesentation.
-     */
-    public String balancedParenthesesRepresentation() {
-
-	String representation = "";
-
-	/*
-	  Hier Loesung einfuegen.
+	/**
+	 * Konstruktor fuer einen Teilbaum ohne Kinder.
+	 * 
+	 * @param data
+	 *            Referenz auf das im Datenfeld zu speichernde Objekt.
 	 */
+	BinaryTree(T data) {
+		this(data, null, null);
+	}
 
-	return representation;
-   }
+	/**
+	 * Konstruktor fuer einen Teilbaum ohne Kinder.
+	 * 
+	 * @param data
+	 *            Referenz auf das im Datenfeld zu speichernde Objekt.
+	 * @param leftChild
+	 *            Referenz auf den linken Teilbaum.
+	 * @param rightChild
+	 *            Referenz auf den rechten Teilbaum.
+	 */
+	BinaryTree(T data, BinaryTree<T> leftChild, BinaryTree<T> rightChild) {
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+		this.data = data;
+	}
+
+	/**
+	 * Gibt eine Referenz auf das im Datenfeld verwaltete Objekt zurueck.
+	 * 
+	 * @return Referenz auf das im Datenfeld verwaltete Objekt.
+	 */
+	public T getData() {
+		return data;
+	}
+
+	/**
+	 * Speichert die uebergebene Referenz im Datenfeld.
+	 * 
+	 * @param Neu
+	 *            zu speicherende Objektreferenz.
+	 */
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	/**
+	 * Gibt eine Referenz auf den linken Teilbaum zurueck.
+	 * 
+	 * @return Referenz auf den linken Teilbaum.
+	 */
+	public BinaryTree<T> getLeftChild() {
+		return leftChild;
+	}
+
+	/**
+	 * Setzt einen neuen linken Teilbaum.
+	 * 
+	 * @param Referenz
+	 *            auf den neuen linken Teilbaum.
+	 */
+	public void setLeftChild(BinaryTree<T> leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	/**
+	 * Gibt eine Referenz auf den rechten Teilbaum zurueck.
+	 * 
+	 * @return Referenz auf den rechten Teilbaum.
+	 */
+	public BinaryTree<T> getRightChild() {
+		return rightChild;
+	}
+
+	/**
+	 * Setzt einen neuen rechten Teilbaum.
+	 * 
+	 * @param Referenz
+	 *            auf den neuen rechten Teilbaum.
+	 */
+	public void setRightChild(BinaryTree<T> rightChild) {
+		this.rightChild = rightChild;
+	}
+
+	/**
+	 * Erzeugt eine "balanced parentheses"-Repraesentation des am aktuellen Knoten gespeicherten Teilbaums inklusive der String-Repraesentation des an der Wurzel verwalteten Datenobjekts.
+	 * 
+	 * @return Textuelle Darstellung der "balanced parentheses"-Repraesentation.
+	 */
+	public String balancedParenthesesRepresentation() {
+
+		String representation = "";
+		representation += generateInner(representation, this);
+	//	System.out.println(representation);
+		return representation;
+
+	}
+
+	private String generateInner(String s, BinaryTree<T> t) {
+		s += "(" + space + t.getData().toString();
+		if (t.getLeftChild() != null) s = this.generateInner(s, t.getLeftChild());
+		if (t.getRightChild() != null) s = this.generateInner(s, t.getRightChild());
+		s += space + ")";
+		return s;
+	}
 
 }
--- Blatt 11/src/g04_C/a43/TreeTest.java
+++ Blatt 11/src/g04_C/a43/TreeTest.java
@@ -1,93 +1,101 @@
 package g04_C.a43;
 
+import org.w3c.dom.Node;
+
 /**
- *  Testklasse zu Aufgabe 43 / Blatt 11 / Sommersemester 2013
+ * Testklasse zu Aufgabe 43 / Blatt 11 / Sommersemester 2013
  */
 public class TreeTest {
 
+	/**
+	 * Erzeugt aus einem Feld, das die Inorder-Traversierung eines Binaerbaums B speichert, und einem Feld, das die Postorder- Traversierung von B speichert, eine Baumdarstellung von B.
+	 * 
+	 * @param inorder
+	 *            Feld, das die Inorder-Traversierung von B speichert.
+	 * @param postorder
+	 *            Feld, das die Postorder-Traversierung von B speichert.
+	 * @return Baumdarstellung von B.
+	 */
+	public BinaryTree<Character> createTree(Character[] inorder, Character[] postorder) {
+		BinaryTree<Character> root;
+		if ((inorder.length == 0) || (inorder.length != postorder.length)) return null;
 
-    /**
-     *  Erzeugt aus einem Feld, das die Inorder-Traversierung eines
-     *  Binaerbaums B speichert, und einem Feld, das die Postorder-
-     *  Traversierung von B speichert, eine Baumdarstellung von B.
-     *  @param inorder Feld, das die Inorder-Traversierung von B speichert.
-     *  @param postorder Feld, das die Postorder-Traversierung von B speichert.
-     *  @return Baumdarstellung von B.
-     */
-    public BinaryTree<Character> createTree(Character[] inorder, Character[] postorder) {
-
-	//  Der Einfachheit halber darf angenommen werden, dass die
-	//  Eingabe-Parameter gueltig sind.
-
-	/*
-	  Hier Loesung einfuegen.
-	*/
-
-	return null;
-
-    }
-
-    public void run() {
-
-	boolean passed = true;
-
-	//  Beispielbaum aus Aufgabeteil (a)
-	Character[] inorder1   = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
-	Character[] postorder1 = {'A', 'C', 'E', 'D', 'B', 'G', 'I', 'H', 'F'};
-	BinaryTree<Character> r1 = createTree(inorder1, postorder1);
-
-	passed = passed && r1.balancedParenthesesRepresentation().equals("(F(B(A)(D(C)(E)))(H(G)(I)))");
-	
-	//  Baum mit genau einem Knoten.
-	Character[] inorder2   = {'A'};
-	Character[] postorder2 = {'A'};
-	BinaryTree<Character> r2 = createTree(inorder2, postorder2);
-
-	passed = passed && r2.balancedParenthesesRepresentation().equals("(A)");
-
-	//  Lineare Liste (nach links) mit je einem Knoten nach rechts (-> strikt)
-	Character[] inorder3   = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
-	Character[] postorder3 = {'A', 'C', 'B', 'E', 'D', 'G', 'F', 'I', 'H'};
-	BinaryTree<Character> r3 = createTree(inorder3, postorder3);
-
-	passed = passed && r3.balancedParenthesesRepresentation().equals("(H(F(D(B(A)(C))(E))(G))(I))");
-
-	//  Lineare Liste (nach rechts) mit je einem Knoten nach links (-> strikt)
-	Character[] inorder4   = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
-	Character[] postorder4 = {'A', 'C', 'E', 'G', 'I', 'H', 'F', 'D', 'B'};
-	BinaryTree<Character> r4 = createTree(inorder4, postorder4);
-
-	passed = passed && r4.balancedParenthesesRepresentation().equals("(B(A)(D(C)(F(E)(H(G)(I)))))");
-
-	//  Zick-Zack-Liste (links-rechts)
-	Character[] inorder5   = {'A', 'B', 'C', 'D', 'E', 'F'};
-	Character[] postorder5 = {'B', 'D', 'C', 'A', 'F', 'E'};
-	BinaryTree<Character> r5 = createTree(inorder5, postorder5);
-
-	passed = passed && r5.balancedParenthesesRepresentation().equals("(E(A(C(B)(D)))(F))");
-
-	//  Vollstaendiger Binaerbaum
-	Character[] inorder6   = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-	Character[] postorder6 = {'A', 'C', 'B', 'E', 'G', 'F', 'D'};
-	BinaryTree<Character> r6 = createTree(inorder6, postorder6);
-
-	passed = passed && r6.balancedParenthesesRepresentation().equals("(D(B(A)(C))(F(E)(G)))");
-
-	if (passed) {
-	    System.out.println("Es wurden alle Tests bestanden.");
-	}
-	else {
-	    System.out.println("Es wurden nicht alle Tests bestanden.");
+		root = constructTree(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1);
+		return root;
 	}
 
-    }
+	private BinaryTree<Character> constructTree(Character[] postorder, int p1, int p2, Character[] inorder, int i1, int i2) {
+		BinaryTree<Character> root = new BinaryTree<Character>(postorder[p2]);
 
-    public static void main(String[] Aufrufparameter) {
+		for (int i = i1; i <= i2; i++) {
+			if (postorder[p2] == inorder[i]) {
+				if (i > i1) root.setLeftChild(constructTree(postorder, p1, p1 + (i - i1 - 1), inorder, i1, i - 1));
+				if (i2 > i) root.setRightChild(constructTree(postorder, p1 + (i - i1), p2 - 1, inorder, i + 1, i2));
+				break;
+			}
+		}
+		return root;
+	}
 
-	TreeTest tt = new TreeTest();
-	tt.run();
+	public void run() {
 
-    }
+		boolean passed = true;
+
+		// Beispielbaum aus Aufgabeteil (a)
+		Character[] inorder1 = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
+		Character[] postorder1 = { 'A', 'C', 'E', 'D', 'B', 'G', 'I', 'H', 'F' };
+		BinaryTree<Character> r1 = createTree(inorder1, postorder1);
+
+		passed = passed && r1.balancedParenthesesRepresentation().equals("(F(B(A)(D(C)(E)))(H(G)(I)))");
+
+		// Baum mit genau einem Knoten.
+		Character[] inorder2 = { 'A' };
+		Character[] postorder2 = { 'A' };
+		BinaryTree<Character> r2 = createTree(inorder2, postorder2);
+
+		passed = passed && r2.balancedParenthesesRepresentation().equals("(A)");
+
+		// Lineare Liste (nach links) mit je einem Knoten nach rechts (-> strikt)
+		Character[] inorder3 = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
+		Character[] postorder3 = { 'A', 'C', 'B', 'E', 'D', 'G', 'F', 'I', 'H' };
+		BinaryTree<Character> r3 = createTree(inorder3, postorder3);
+
+		passed = passed && r3.balancedParenthesesRepresentation().equals("(H(F(D(B(A)(C))(E))(G))(I))");
+
+		// Lineare Liste (nach rechts) mit je einem Knoten nach links (-> strikt)
+		Character[] inorder4 = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
+		Character[] postorder4 = { 'A', 'C', 'E', 'G', 'I', 'H', 'F', 'D', 'B' };
+		BinaryTree<Character> r4 = createTree(inorder4, postorder4);
+
+		passed = passed && r4.balancedParenthesesRepresentation().equals("(B(A)(D(C)(F(E)(H(G)(I)))))");
+
+		// Zick-Zack-Liste (links-rechts)
+		Character[] inorder5 = { 'A', 'B', 'C', 'D', 'E', 'F' };
+		Character[] postorder5 = { 'B', 'D', 'C', 'A', 'F', 'E' };
+		BinaryTree<Character> r5 = createTree(inorder5, postorder5);
+
+		passed = passed && r5.balancedParenthesesRepresentation().equals("(E(A(C(B)(D)))(F))");
+
+		// Vollstaendiger Binaerbaum
+		Character[] inorder6 = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+		Character[] postorder6 = { 'A', 'C', 'B', 'E', 'G', 'F', 'D' };
+		BinaryTree<Character> r6 = createTree(inorder6, postorder6);
+
+		passed = passed && r6.balancedParenthesesRepresentation().equals("(D(B(A)(C))(F(E)(G)))");
+
+		if (passed) {
+			System.out.println("Es wurden alle Tests bestanden.");
+		} else {
+			System.out.println("Es wurden nicht alle Tests bestanden.");
+		}
+
+	}
+
+	public static void main(String[] Aufrufparameter) {
+
+		TreeTest tt = new TreeTest();
+		tt.run();
+
+	}
 
 }
-
